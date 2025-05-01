57e881260b4ed12a3156c1fd6f69a8cd62c17b0c
George Owen <gowen@esri.com>
2025-05-01 13:13:25 -0700

    init eslint, prettier

--- src/VectorTileService.ts
+++ src/VectorTileService.ts
@@ -1,1 +1,28 @@
 import type {SourceSpecification} from '@maplibre/maplibre-gl-style-spec';
+/*
+map.addSource
+
+map.addLayer
+
+maplibregl.Esri.vectorTileSource(itemId/URL)
+
+needs to handle:
+
+source-layer property
+source property
+*/
+export interface Source {
+    readonly type: string;
+
+    id: string;
+
+    minzoom: number;
+    maxzoom: number;
+}
+
+
+interface VectorTileSource {
+    attribution: string;
+    type: "vector";
+    
+}
