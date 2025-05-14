cec5eb7ac61ec0710ddcf43340af7aabbea06265
George Owen <gowen@esri.com>
2025-05-13 17:00:21 -0700

    minimize option for attribution control

--- src/EsriAttributionControl.ts
+++ src/EsriAttributionControl.ts
@@ -1,18 +1,32 @@
-import { AttributionControl } from "maplibre-gl";
+import { AttributionControl, AttributionControlOptions, Map } from "maplibre-gl";
 
 interface EsriAttributionControlOptions {
-    compact?: boolean;
+    compact?:boolean;
+    minimize?: boolean;
 }
 
 export class EsriAttributionControl extends AttributionControl {
+    _minimized?:boolean;
     constructor (options : EsriAttributionControlOptions) {
-
+        
         const esriAttribution = "Powered by <a href=\"https://www.esri.com/\" target=\"_blank\">Esri</a>";
         const maplibreAttribution = "<a href=\"https://maplibre.org/\" target=\"_blank\">MapLibre</a>";
-        super({
+
+        const attributionOptions : AttributionControlOptions = {
             compact: (options?.compact !== undefined) ? options.compact : true,
             customAttribution: esriAttribution //`${maplibreAttribution} | ${esriAttribution}`
-        });
+        }
+
+        super(attributionOptions);
+        this._minimized = options?.minimize;
+    }
+    onAdd(map : Map) {
+        const htmlElement = super.onAdd(map);
+
+        if (this._minimized) {
+            this._updateCompactMinimize();
+        }
+        return htmlElement;
     }
 }
 
