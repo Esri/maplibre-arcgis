9d031a044b7118e2e6f1b7a6fce85ea1eb70840d
George Owen <gowen@esri.com>
2025-07-08 19:15:14 -0700

    type extensions for rest js

---
+++ src/@types/typings.d.ts
@@ -0,0 +1,28 @@
+import type { IRequestOptions } from "@esri/arcgis-rest-request";
+
+declare module '@esri/arcgis-rest-portal' {
+    type IItemResourcesResponse = {
+        start:number;
+        nextStart:number;
+        num:number;
+        total:number;
+        resources: [{
+            resource:string;
+            access:string;
+            created:number;
+            size:number;
+        }]
+    };
+
+    type IVectorTileServiceDefinition = {
+        tiles: [string];
+        defaultStyles: string;
+        copyrightText: string;
+    };
+
+    interface IItem {
+        accessInformation:string;
+    }
+    
+    function getItemResources(id: string, requestOptions?: IRequestOptions): Promise<IItemResourcesResponse>
+}
