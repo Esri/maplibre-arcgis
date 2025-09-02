82fa1efeec02f5bb49141dfb54fba53fdc9e533f
George Owen <gowen@esri.com>
2025-09-02 09:50:54 -0700

    update constructor options

--- src/BasemapStyle.ts
+++ src/BasemapStyle.ts
@@ -75,10 +75,6 @@ export interface IBasemapStyleOptions {
    */
   preferences?: IBasemapPreferences;
   /**
-   * The maplibre-gl map to apply the basemap style to.
-   */
-  map?: Map;
-  /**
    * Options for customizing the maplibre-gl attribution control.
    */
   attributionControl?: EsriAttributionControlOptions;
@@ -92,6 +88,10 @@ export interface IBasemapStyleOptions {
  * Options for applyStyle
  */
 export interface IApplyStyleOptions extends IBasemapStyleOptions {
+  /**
+   * The maplibre-gl map to apply the basemap style to.
+   */
+  map?: Map;
   /**
    * Passthrough options for maplibre-gl map.setStyle()
    */
@@ -184,8 +184,6 @@ export class BasemapStyle {
     else throw new Error(
       'ArcGIS access token required. To learn more, go to https://developers.arcgis.com/documentation/security-and-authentication/get-started/.'
     );
-
-    if (options.map) this._map = options.map;
 
     this.styleId = options.style;
     this._baseUrl = options?.baseUrl || DEFAULT_BASE_URL;
@@ -451,7 +449,6 @@ export class BasemapStyle {
    */
   static applyStyle(map: Map, options: IApplyStyleOptions): BasemapStyle {
     if (!map) throw new Error('Must provide a maplibre-gl \'Map\' to apply style to.');
-    options.map = map;
 
     const basemapStyle = new BasemapStyle(options);
 
