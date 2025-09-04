81cec2cb3566b04351401607c8b97a926b9969ef
Mark Torrey <MarkTorrey@users.noreply.github.com>
2025-09-04 08:29:37 -0400

    Update src/HostedLayer.ts
    Co-authored-by: gowin <gowen@esri.com>

--- src/HostedLayer.ts
+++ src/HostedLayer.ts
@@ -74,7 +74,7 @@ const throwReadOnlyError = (propertyName: string) => {
 
 /**
  * Abstract class representing a [hosted layer](https://developers.arcgis.com/documentation/portal-and-data-services/data-services/types-of-data-services/) for MapLibre GL JS.
- * This class provides a common interface for layers that are hosted on ArcGIS, such as feature layers, tile layers, and basemap styles.
+ * This class provides a common base for loading data hosted in ArcGIS, such as feature layers and vector tile layers. It cannot be instantiated directly.
  * It includes methods for managing authentication, sources, layers, and adding them to a MapLibre map.
  * Subclasses must implement the `initialize` method to load data from ArcGIS.
  */
