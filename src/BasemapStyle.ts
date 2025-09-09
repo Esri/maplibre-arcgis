8d8662c5d2357bc32dfa9597921281291c8ef15a
Mark Torrey <MarkTorrey@users.noreply.github.com>
2025-09-09 13:27:50 -0400

    Update src/BasemapStyle.ts
    Co-authored-by: Sheryl Tania <sherylgiovanni@gmail.com>

--- src/BasemapStyle.ts
+++ src/BasemapStyle.ts
@@ -218,7 +218,7 @@ export class BasemapStyle {
    *   preferences: {
    *     language: 'en',
    *     worldview: 'unitedStatesOfAmerica',
-   *     places: 'attributed
+   *     places: 'attributed'
    *   }
    * });
    *
