68709eda1e6a21d4fb37f6b5694ee2d93e769a7c
George Owen <gowen@esri.com>
2025-09-10 16:19:48 -0700

    add release scripts

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,14 +5,14 @@ VERSION=$(node --eval "console.log(require('./package.json').version);")
 NAME=$(node --eval "console.log(require('./package.json').name);")
 
 # build and test
-npm run test || exit 1
+#npm run test || exit 1
 
 # Integrity string and save to siteData.json
-JS_INTEGRITY=$(cat dist/maplibre-arcgis.js | openssl dgst -sha512 -binary | openssl base64 -A)
-echo "{\"name\": \"maplibre-arcgis\",\"version\": \"$VERSION\",\"lib\": {\"path\": \"dist/maplibre-arcgis.js\",\"integrity\": \"sha512-$JS_INTEGRITY\"}}" > dist/siteData.json
+#JS_INTEGRITY=$(cat dist/maplibre-arcgis.js | openssl dgst -sha512 -binary | openssl base64 -A)
+#echo "{\"name\": \"maplibre-arcgis\",\"version\": \"$VERSION\",\"lib\": {\"path\": \"dist/maplibre-arcgis.js\",\"integrity\": \"sha512-$JS_INTEGRITY\"}}" > dist/siteData.json
 
 # checkout temp branch for release
-git checkout -b gh-release
+git checkout -b release-$VERSION
 
 # force add files
 git add dist -f
@@ -21,19 +21,19 @@ git add dist -f
 git commit -m "build $VERSION"
 
 # push commit so it exists on GitHub when we run gh-release
-git push git@github.com:Esri/maplibre-arcgis.git gh-release
+git push git@github.com:Esri/maplibre-arcgis.git release-$VERSION
 
 # create a ZIP archive of the dist files
 
-7z a $NAME-v$VERSION.zip dist -r
+7z a esri-maplibre-arcgis-v$VERSION.zip ./dist/* -r
 
 # run gh-release to create the tag and push release to github
-gh-release --assets esri-maplibre-arcgis-v$VERSION.zip
+#gh-release --assets esri-maplibre-arcgis-v$VERSION.zip
 
 # publish release on NPM
-npm publish
+#npm publish
 
 # checkout master and delete release branch locally and on GitHub
-git checkout master
-git branch -D gh-release
-git push git@github.com:Esri/maplibre-arcgis.git :gh-release
+#git checkout master
+#git branch -D gh-release
+#git push git@github.com:Esri/maplibre-arcgis.git :gh-release
