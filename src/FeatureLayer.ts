453c7a9a411e27a431a224a53bf39b6de4d2be3c
George Owen <gowen@esri.com>
2025-09-10 10:01:41 -0700

    add exports field

--- src/FeatureLayer.ts
+++ src/FeatureLayer.ts
@@ -173,7 +173,7 @@ export class FeatureLayer extends HostedLayer {
     }
     else {
       throw new Error(
-        'Feature layers hosted in old versions of ArcGIS Enterprise are not currently supported in this plugin. Support will be added in a future release: https://github.com/ArcGIS/maplibre-arcgis/issues/5'
+        'Feature layers hosted in old versions of ArcGIS Enterprise are not currently supported in this plugin. Support will be added in a future release: https://github.com/Esri/maplibre-arcgis/issues/5'
       );
     }
     if (!layerData) throw new Error('Unable to load data.');
