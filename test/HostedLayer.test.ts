1cb1a9949fe788cb5acb3152c2e445be3a06887f
George Owen <gowen@esri.com>
2025-09-11 13:56:32 -0700

    vtl parameter tests

--- test/HostedLayer.test.ts
+++ test/HostedLayer.test.ts
@@ -3,8 +3,8 @@ import { describe, expect, vi, beforeAll, beforeEach } from 'vitest';
 import { customTest as test } from './BaseTest';
 import { HostedLayer } from '../src/HostedLayer';
 
-test.only('Cannot be instantiated directly.', () => {
+test('Cannot be instantiated directly.', () => {
   expect(() => {
     const abstractLayer = new HostedLayer({});
-  }).toThrowError();
+  }).toThrowError('HostedLayer is an abstract class and cannot be instantiated directly.');
 });
--- test/VectorTileLayer.test.ts
+++ test/VectorTileLayer.test.ts
@@ -4,6 +4,9 @@ import { customTest as test } from './BaseTest'
 import { useMock, removeMock } from './setupUnit';
 import { VectorTileLayer } from '../src/VectorTileLayer';
 
+const itemIdUSA = '31eb749371c441e0b3ac5db4f60ecba9';
+const serviceUrlParcels = 'https://vectortileservices3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Santa_Monica_Mountains_Parcels_VTL/VectorTileServer';
+
 describe('Vector tile layer tests', () => {
   beforeAll(() => {
     useMock();
@@ -13,28 +16,116 @@ describe('Vector tile layer tests', () => {
     fetchMock.resetMocks();
   });
 
-  test('Throws if neither an item ID nor service URL are provided.', () => {});
-  test('Throws if the URL is invalid.', () => {});
+  test('Throws if neither an item ID nor service URL are provided.', () => {
+    expect(() => {
+      const vtl = new VectorTileLayer({});
+    }).toThrowError('Vector tile layer requires either an \'itemId\' or \'url\'.');
 
-  describe('Works with secure layers', () => {
-    test('Accepts authentication as an access token string.', () => {});
-    test('Accepts authentication as a REST JS object.', () => {});
+    expect(() => {
+      const vtl = new VectorTileLayer();
+    }).toThrowError('Vector tile layer requires either an \'itemId\' or \'url\'.');
+  });
 
+  describe('Works with authenticated layers', () => {
+    test('Accepts authentication as an access token string.', ({apiKey}) => {
+      const vtl = new VectorTileLayer({
+        url: serviceUrlParcels,
+        token: apiKey
+      });
+      expect(typeof vtl.authentication).toBe('string');
+      expect(vtl.token).toBe(apiKey);
+    });
+
+    test('Accepts authentication as a REST JS object.', ({restJsAuthentication}) => {
+      const vtl = new VectorTileLayer({
+        url: serviceUrlParcels,
+        authentication: restJsAuthentication
+      });
+      expect(typeof vtl.authentication).toBe('object');
+      expect(vtl.token).toBe(restJsAuthentication.token);
+    });
+
+    // TODO
     test('Loads a style from a secure item ID.',()=>{});
     test('Loads a style from a secure service URL', ()=>{});
     test('Adds authentication to the style tile URLs, sprites, and glyphs.', () => {});
   });
 
   describe('Loads data from a service URL', () => {
-    test('Accepts a service URL in the constructor.', () => {});
+    test('Accepts a service URL in the constructor and cleans the URL.', () => {
+      const vtl = new VectorTileLayer({
+        url: serviceUrlParcels
+      });
+      expect(vtl._inputType).toBe('VectorTileService');
+      expect(vtl._serviceInfo).toEqual({
+        serviceUrl: serviceUrlParcels + '/'
+      })
+    });
+
+    test('Throws if the URL is not the URL of a vector tile service.', () => {
+      // Not a URL
+      expect(() => {
+        const vtl = new VectorTileLayer({
+          url: itemIdUSA
+        });
+      }).toThrowError('Argument `url` is not a valid vector tile service URL.');
+
+      // Not a vector tile service
+      expect(() => {
+        const vtl = new VectorTileLayer({
+          url: 'https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trails/FeatureServer/'
+        });
+      }).toThrowError('Argument `url` is not a valid vector tile service URL.');
+    });
+
+    // TODO
     test('Fetches the style from a service URL\'s default resource.', () => {});
     test('Fetches a style with a custom resource name.', () => {});
     test('Fetches service metadata, including the service name and attribution.', () => {});
   });
 
   describe('Loads data from an item ID', () => {
-    test('Accepts an item ID in the constructor.', () => {});
-    test('Prefers an item ID over a service URL if both are provided.', () => {});
+    test('Accepts an item ID and portal URL in the constructor, and the portal URL defaults to arcgis.com.', () => {
+      const vtl = new VectorTileLayer({
+        itemId: itemIdUSA
+      });
+      expect(vtl._inputType).toBe('ItemId');
+      expect(vtl._itemInfo).toEqual({
+        itemId: itemIdUSA,
+        portalUrl: 'https://www.arcgis.com/sharing/rest'
+      });
+
+      // Custom portal URL
+      const vtl2 = new VectorTileLayer({
+        itemId: itemIdUSA,
+        portalUrl: 'https://my-evil-portal.com/items'
+      });
+      expect(vtl2._itemInfo).toEqual({
+        itemId: itemIdUSA,
+        portalUrl: 'https://my-evil-portal.com/items'
+      });
+    });
+
+    test('Throws if the item ID format is invalid', () => {
+      expect(() => {
+        const vtl = new VectorTileLayer({
+          itemId: 'random junk'
+        });
+      }).toThrowError('Argument `itemId` is not a valid item ID.')
+    })
+
+    test('Prefers an item ID over a service URL if both are provided.', () => {
+      const warningSpy = vi.spyOn(console, 'warn');
+      const vtl = new VectorTileLayer({
+        itemId: itemIdUSA,
+        url: serviceUrlParcels
+      });
+      expect(warningSpy).toHaveBeenCalledWith('Both an item ID and service URL have been passed. Only the item ID will be used.');
+      expect(vtl._inputType).toBe('ItemId');
+      expect(vtl._serviceInfo).toBeUndefined();
+    });
+
+    // TODO
     test('Fetches the style of an item ID from the item resources.', () => {});
     test('Prefers the correct style when multiple style resources are present.', () => {});
     test('Falls back to the style of the service URL if no style resource is found on the item.', () => {});
@@ -43,6 +134,7 @@ describe('Vector tile layer tests', () => {
     test('Fetches item metadata, including the layer name and attribution.', () => {});
   });
 
+  // TODO
   describe('Formats a style properly for use with MapLibre.', () => {
     test('Formats the source URL.', () => {});
     test('Creates a \'tiles\' property with information from the service.', () => {});
@@ -59,12 +151,14 @@ describe('Vector tile layer tests', () => {
   });
 
 
+  // TODO
   test('Displays layer attribution on the map.', () => {});
   test('Creates a layer from item ID with the `fromPortalItem` static method.', () => {});
   test('Creates a layer from service URL with the `fromUrl` static method.', () => {});
   test('Uninitialized layers created using the constructor cannot be added to the map.', ()=>{});
   test('Loads style data on an uninitialized layer using the `initialize()` method.', () => {});
 
+  // TODO
   describe('Methods inherited from HostedLayer work properly.', () => {
     test('`addSourcesTo` adds sources to the maplibre map.', () => {});
     test('`addLayersTo` adds layers to the maplibre map.', () => {});
